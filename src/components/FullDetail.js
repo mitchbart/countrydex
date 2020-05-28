fe4eccdad43c8d500cd55833ea7b4489821bbc29
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2020-05-28 14:52:25 +1000

    added google maps and timezones

--- src/components/FullDetail.js
+++ src/components/FullDetail.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import CountryCard from './CountryCard'
+import Map from './Map'
 
 export default function FullDetail({ showOne, setShowOne, countryData }) {
   console.log(showOne)
   const neighbors = showOne.borders
   console.log(neighbors)
   //console.log(showOne.languages.length)
+  const options = {
+    center: { lat: showOne.latlng[0], lng: showOne.latlng[1] },
+    zoom: 5,
+  }
   return (
     <div>
       <button onClick={() => setShowOne("")}>back</button>
@@ -30,12 +35,26 @@ export default function FullDetail({ showOne, setShowOne, countryData }) {
       {showOne.currencies.length === 1 && <p>Currency: {showOne.currencies[0].name} | {showOne.currencies[0].symbol}</p>}
       {showOne.currencies.length > 1 && 
         <div>
-          <p>Languages</p>
+          <p>Local currencies</p>
           <ul>
             {showOne.currencies.map(currency => <li key={currency.code}>{currency.name} | {currency.symbol}</li>)}
           </ul>
         </div>
       }
+
+      {showOne.timezones.length === 1 && <p>Timezone: {showOne.timezones[0]}</p>}
+      {showOne.timezones.length > 1 && 
+        <div>
+          <p>Timezones</p>
+          <ul>
+            {showOne.timezones.map(timezone => <li key={timezone}>{timezone}</li>)}
+          </ul>
+        </div>
+      }
+
+      <Map
+        options={options}
+      />
 
       {showOne.borders.length > 0 && 
         <div>
---
+++ src/components/Map.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+export default function Map({ options, onMount, className, onMountProps }) {
+  const ref = useRef()
+  const [map, setMap] = useState()
+
+  useEffect(() => {
+    // const onLoad = () => setMap(new window.google.maps.Map(ref.current, options))
+    const onLoad = () => setMap(new window.google.maps.Map(ref.current, options))
+    if (!window.google) {
+      const script = document.createElement(`script`)
+      script.src =
+        `https://maps.googleapis.com/maps/api/js?key=` +
+        process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+      document.head.append(script)
+      script.addEventListener(`load`, onLoad)
+      return () => script.removeEventListener(`load`, onLoad)
+    } else onLoad()
+  }, [options])
+
+  if (map && typeof onMount === `function`) onMount(map, onMountProps)
+
+  return (
+    <div
+      style={{ height: `60vh`, margin: `1em 0`, borderRadius: `0.5em` }}
+      {...{ ref, className }}
+    />
+  )
+}
+
